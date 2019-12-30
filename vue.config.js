2c1f251ec1489ac3b7ebef6cc32ef6e333fa33fa
Kitamura <hinitial@gmail.com>
2019-12-30 21:41:52 +0900

    config はこの名前で追加するみたい

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    index: {
+        // entry for the page
+        entry: 'src/index/main.js',
+        // the source template
+        template: 'public/index.html',
+        // output as dist/index.html
+        filename: 'index.html',
+        // when using title option,
+        // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+        title: 'テストページ',
+        // chunks to include on this page, by default includes
+        // extracted common chunks and vendor chunks.
+        chunks: ['chunk-vendors', 'chunk-common', 'index']
+    }
+};
